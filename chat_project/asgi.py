227e6a56ae06ab6d99fe71a63c3553b4f3ac064a
Semin Ju <seminju0172@gmail.com>
2025-01-14 20:11:52 +0900

    Feat: JWTAuthMiddleware 추가
    - 웹소켓이 JWT 인증을 위한 JWTAuthMiddleware 추가
    - JWTAuthMiddleware 사용을 위한 asgi.py 수정

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -1,14 +1,14 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from middlewares.jwt_middleware import JWTAuthMiddleware
 from user import routing as user_routing  # user 앱의 routing.py 임포트
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),  # HTTP 요청 처리
-    "websocket": AuthMiddlewareStack(  # 기본 인증 미들웨어
+    "websocket": JWTAuthMiddleware(
         URLRouter([
             # user 앱의 WebSocket 경로
             *user_routing.websocket_urlpatterns,
