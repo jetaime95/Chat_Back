ed7880b80411105662225c80eaaf01c572783cbe
Semin Ju <seminju0172@gmail.com>
2025-03-24 10:26:37 +0900

    Fix: settings.py 수정

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -138,7 +138,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DATABASE_NAME', ''),
         'USER': os.environ.get('DATABASE_USER', ''),
-        'PASSWORD': os.environ.get('DATABASE_PWD', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
         'HOST': os.environ.get('DATABASE_HOST', ''),
         'PORT': os.environ.get('DATABASE_PORT', ''),
     }
